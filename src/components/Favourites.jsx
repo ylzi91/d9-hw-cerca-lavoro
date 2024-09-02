1f254aa6ed9de1ae427204597b3c01da757fd086
ylzi91 <97354085+ylzi91@users.noreply.github.com>
2024-09-02 17:12:40 +0200

    Elimina da favorite

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,10 +1,11 @@
 import { Container, Row, Col, Button } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 
 
 export default function Favurites (){
+    const dispach = useDispatch() 
 
     const arrayFavorite = useSelector((state) => {
         return state.favourites.jobs
@@ -21,7 +22,12 @@ export default function Favurites (){
                         <Link to={`/${fav.company_name}`}>{fav.company_name}</Link>
                     </Col>
                     <Col xs={6} className="mx-auto d-flex justify-content-between">
-                        <Button variant="danger">Elimina</Button>
+                        <Button variant="danger" onClick={()=> {
+                            dispach({
+                                type: 'REMOVE_TO_FAVORITES',
+                                payload: fav._id
+                            })
+                        }}>Elimina</Button>
                     </Col>
                     </>
                     )
